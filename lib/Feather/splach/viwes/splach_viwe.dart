a6407c47bdc1f7b391f28fa0de1d1262234af804
Mohamed <mohmedabrhem981@gmail.com>
2026-07-29 03:15:08 +0300

    creat project

---
+++ lib/Feather/splach/viwes/splach_viwe.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplachViwe extends StatelessWidget {
+  const SplachViwe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:fashion_store/Feather/splach/viwes/splach_viwe.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const fashion());
+}
+class fashion extends StatelessWidget {
+  const fashion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,home: SplachViwe(),);
+  }
+}
